669bf95575b6e79166cc2c2a79a41a185709d38c
Matheus Dias Pichler <matheuspichler94@gmail.com>
2025-09-03 21:22:01 -0300

    Aplicando o Lombok

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
@@ -2,6 +2,9 @@ package dev.java10x.CadastroDeNinjas.Ninjas;
 
 import dev.java10x.CadastroDeNinjas.Missoes.MissoesModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
@@ -9,56 +12,26 @@ import java.util.List;
 // JPA = Java Persistence API
 @Entity
 @Table(name = "tb_cadastro ")
+@NoArgsConstructor // Cria o construtor vazio
+@AllArgsConstructor // Cria o construtor com todos os argumentos.
+@Data // Cria os getter and setters
+
 public class NinjaModel {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-   private Long id;
+    private Long id;
 
-   private String nome;
+    private String nome;
 
-   private String email;
+    private String email;
 
-   private int idade;
+    private int idade;
 
-   // @ManyToOne Um ninja tem uma unica missão
-   @ManyToMany
-   @JoinColumn(name = "missoes_id") //Foreing key
-   private MissoesModel missoes;
-
-
-    public NinjaModel() {
-    }
-
-    public NinjaModel(String nome, String email, int idade) {
-        this.nome = nome;
-        this.email = email;
-        this.idade = idade;
-    }
+    // @ManyToOne Um ninja tem uma unica missão
+    @ManyToMany
+    @JoinColumn(name = "missoes_id") //Foreing key
+    private MissoesModel missoes;
 
 
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
 }
